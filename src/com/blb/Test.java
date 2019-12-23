e4adb84fb2e6da7a13810b32d4bc14fffbc73d00
hms <xianxianxian80@gmail.com>
2019-12-23 17:24:28 +0800

    做了个system

--- src/com/blb/Test.java
+++ src/com/blb/Test.java
@@ -1,12 +1,12 @@
 package com.blb;
 
-import java.util.Arrays;
+
 
 public class Test {
     public static void main(String[] args){
         System.out.println("1521");
         System.out.println("hello");
-         System.out.println("world");
-
+        System.out.println("world");
+        System.out.println("I like beautiful girls");
     }
 }
