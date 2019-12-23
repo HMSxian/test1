b41eb326e4d012b4b48c5fbdc936d6414d957c34
hms <xianxianxian80@gmail.com>
2019-12-23 17:37:27 +0800

    第一次提交

--- src/com/blb/Test.java
+++ src/com/blb/Test.java
@@ -4,10 +4,6 @@ package com.blb;
 
 public class Test {
     public static void main(String[] args){
-        System.out.println("1521");
-        System.out.println("hello");
-        System.out.println("world");
-        System.out.println("girl");
-        System.out.println("yingying");
+        int a;
     }
 }
