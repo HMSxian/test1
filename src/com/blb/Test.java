d1b2de6e862c53cec4035f44f01cf5e2eb23416e
HMSxian <59161956+HMSxian@users.noreply.github.com>
2019-12-23 17:14:40 +0800

    Update Test.java

--- src/com/blb/Test.java
+++ src/com/blb/Test.java
@@ -6,6 +6,7 @@ public class Test {
     public static void main(String[] args){
         System.out.println("1521");
         System.out.println("hello");
+         System.out.println("world");
 
     }
 }
